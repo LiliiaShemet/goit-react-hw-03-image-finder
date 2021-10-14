a126fe8f1ad9600597c89ee829817e83721ace19
LiliiaShemet <78089366+LiliiaShemet@users.noreply.github.com>
2021-10-14 23:16:27 +0300

    добавляет проптайпс

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,8 +1,12 @@
 import React from "react";
 import s from "./Section.module.css";
+import PropTypes from 'prop-types';
 
 const Section = ({ children }) => {
   return <section className={s.Gallery}>{children}</section>;
+};
+Section.propTypes = {
+  title: PropTypes.string.isRequired
 };
 
 export default Section;
