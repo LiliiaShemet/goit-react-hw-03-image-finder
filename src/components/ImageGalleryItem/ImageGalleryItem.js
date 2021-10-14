fc2c6d013e91823d9328ed06c3a4cde75e833bce
LiliiaShemet <78089366+LiliiaShemet@users.noreply.github.com>
2021-10-14 22:58:25 +0300

    Менет класовый компонент на функцию, добавляет mapper

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,34 +1,24 @@
 import PropTypes from "prop-types";
 import { Component } from "react";
 import s from "./ImageGalleryItem.module.css";
-
-class ImageGalleryItem extends Component {
-  handleClickImage = e => {
-    this.props.openModal(e.target.dataset.large);
-  };
-
-  render() {
-    const { webformatURL, largeImageURL, tags = "image" } = this.props;
-
-    return (
-      <li className={s.Item}>
-        <img
-          onClick={this.handleClickImage}
-          src={webformatURL}
-          alt={tags}
-          className={s.Image}
-          data-large={largeImageURL}
-        />
-      </li>
-    );
-  }
+function ImageGalleryItem({ webformatURL, largeImageURL, tags, onOpenModal }) {
+  return (
+    <li className={s.item}>
+      <img
+        src={webformatURL}
+        alt={tags}
+        data-source={largeImageURL}
+        className={s.image}
+        onClick={onOpenModal}
+      />
+    </li>
+  );
 }
 
 ImageGalleryItem.propTypes = {
-  webformatURL: PropTypes.string.isRequired,
   largeImageURL: PropTypes.string.isRequired,
   tags: PropTypes.string.isRequired,
-  openModal: PropTypes.func.isRequired,
+  webformatURL: PropTypes.string.isRequired,
 };
 
 export default ImageGalleryItem;
--- src/services/services.js
+++ src/services/services.js
@@ -4,7 +4,18 @@ const fetchImage = (query, page = 1) => {
   return fetch(
     `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   ).then(response => {
-    return response.json();
+    return response.map(
+      ({
+        id: imageId,
+        webformatURL: linkSmallImage,
+        largeImageURL: linkLargeImage
+
+      }) => ({
+        imageId,
+        linkSmallImage,
+        linkLargeImage
+      })
+    );
   });
 };
 
